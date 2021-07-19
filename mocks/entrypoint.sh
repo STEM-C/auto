22d14be9f93fb0ad55cb02b84f7d20d6878b2346
lilyh14 <lhinkeldey@ufl.edu>
2021-07-19 12:48:54 -0400

    Updated entrypoint.sh to only include tests commands

--- mocks/entrypoint.sh
+++ mocks/entrypoint.sh
@@ -15,23 +15,6 @@ github_token=$3
 gpr_base="docker.pkg.github.com/stem-c/casmm"
 heroku_base="registry.heroku.com/$app_name"
 
-# Log into gpr
-echo "$github_token" | docker login docker.pkg.github.com -u "$GITHUB_ACTOR" --password-stdin
-
-# Name and build server image
-gpr_server="$gpr_base/server"
-heroku_server="$heroku_base/web"
-
-docker pull "$gpr_server" || true
-docker build -t "$gpr_server:$image_tag" -t "$gpr_server:latest" -t "$heroku_server" --cache-from "$gpr_server" .
-
-# Name and build compile image
-gpr_compile="$gpr_base/compile"
-heroku_compile="$heroku_base/compile"
-
-docker pull "$gpr_compile" || true
-docker build -t "$gpr_compile:$image_tag" -t "$gpr_compile:latest" -t "$heroku_compile" --cache-from "$gpr_compile" -f ./compile/Dockerfile ./compile
-
 # Test
  docker-compose up -d
  ready=false
@@ -43,16 +26,3 @@ docker build -t "$gpr_compile:$image_tag" -t "$gpr_compile:latest" -t "$heroku_c
  yarn integration
  yarn performance
 
-# Push gpr images
-docker push "$gpr_server"
-docker push "$gpr_compile"
-
-# Login to heroku container registry
-heroku container:login
-
-# Push heroku image
-docker push "$heroku_server"
-docker push "$heroku_compile"
-
-# Deploy app
-heroku container:release -a "$app_name" web compile
