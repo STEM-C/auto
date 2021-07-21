8a887c96c6ca6bf5d3020a3a75e856f78d3bae23
lilyh14 <lhinkeldey@ufl.edu>
2021-07-21 11:15:50 -0400

    Updated entrypoint script
    Deleted more of the bash commands that i didn't need

--- mocks/entrypoint.sh
+++ mocks/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-set -e
+#set -e
 
-if [[ "$#" != 3 ]];
-then 
-    echo "Expecting 3 arguments, got $#!"
-    exit 0
-fi;
+#if [[ "$#" != 3 ]];
+#then 
+#    echo "Expecting 3 arguments, got $#!"
+#    exit 0
+#fi;
 
 #image_tag=$1
 #app_name=$2
-github_token=$3
+#github_token=$3
 
-gpr_base="docker.pkg.github.com/stem-c/casmm"
+#gpr_base="docker.pkg.github.com/stem-c/casmm"
 #heroku_base="registry.heroku.com/$app_name"
 
 # Test
